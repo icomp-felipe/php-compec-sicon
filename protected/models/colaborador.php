31dc80860693749475ea756f418ec5197e93ddcb
Felipe André <fass@icomp.ufam.edu.br>
2021-05-08 14:32:10 -0400

    Controlador 'colaborador' otimizado e comentado

--- protected/models/colaborador.php
+++ protected/models/colaborador.php
@@ -1,65 +1,40 @@
 <?php
 
-class colaborador extends CActiveRecord
-{
-	/**
-	 * The followings are the available columns in table 'colaborador':
-	 * @var integer $idColaborador
-	 * @var integer $idescolaridade
-	 * @var integer $iduf_identidade
-	 * @var integer $idmunicipio
-	 * @var string $nome
-	 * @var string $sexo
-	 * @var string $data_nascimento
-	 * @var string $doc_identidade
-	 * @var string $orgao_identidade
-	 * @var string $logradouro
-	 * @var string $numero_endereco
-	 * @var string $bairro
-	 * @var string $cep
-	 * @var string $ddd
-	 * @var string $telefone
-	 * @var string $celular
-	 * @var string $email
-	 * @var string $cpf
-	 * @var integer $tipo_cadastro
-	 * @var integer $status_cadastro
-	 * @var integer $tipo_vinculo
-	 * @var integer $anoatualgraduacao
-	 * @var integer $matriculaufam
-	 * @var string $cursoufam
-	 * @var integer $matriculaservidor
-	 * @var string $orgaoservidor
-	 * @var string $observacoes
-	 * @var string $banco
-	 * @var string $contacorrente
-	 * @var string $agencia
-	 * @var string $pispasep
-	 * @var integer $tipo_vinculo_old	 
-	 */
+/** Modelagem do colaborador.
+ *  Atualizada em: 08/05/2021
+ *  @author Felipe André <felipeandresouza@hotmail.com>
+ */
+class colaborador extends CActiveRecord {
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @return CActiveRecord the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
+	/** @return CActiveRecord the static model of the specified AR class. */
+	public static function model($className=__CLASS__) {
 		return parent::model($className);
 	}
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
+	/** @return string the associated database table name */
+	public function tableName()	{
 		return 'colaborador';
 	}
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{	
+	/** @return array relational rules. */
+	public function relations()	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'municipio' => array(self::BELONGS_TO, 'municipio', 'idmunicipio'),
+			'iduf_identidade0' => array(self::BELONGS_TO, 'uf', 'iduf_identidade'),
+			'idescolaridade0' => array(self::BELONGS_TO, 'escolaridade', 'idescolaridade'),
+			'envio_emails' => array(self::HAS_MANY, 'envioEmail', 'idColaborador'),
+			'inscricaos' => array(self::HAS_MANY, 'inscricao', 'idColaborador'),
+			'instituicoesDirigidas' => array(self::HAS_MANY, 'instituicao', 'idResponsavel'),
+			'usuarios' => array(self::HAS_MANY, 'usuario', 'idColaborador'),
+			'vinculos' => array(self::HAS_MANY, 'vinculo', 'idColaborador'),
+		);
+	}
+
+	/** @return array validation rules for model attributes. */
+	public function rules()	{
+
 		return array(
 
 			// Identificação
@@ -97,38 +72,10 @@ class colaborador extends CActiveRecord
 			array('tipo_cadastro, status_cadastro, tipo_vinculo', 'numerical', 'integerOnly' => true),
 		);
 	}
-	
-	/** Prepara alguns campos antes de serem validados. */
-	protected function beforeValidate(){
 
-		$this->cpf = str_replace('.','',$this->cpf);
-		$this->cpf = str_replace('-','',$this->cpf);
-
-		return true;
-	}
-
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'municipio' => array(self::BELONGS_TO, 'municipio', 'idmunicipio'),
-			'iduf_identidade0' => array(self::BELONGS_TO, 'uf', 'iduf_identidade'),
-			'idescolaridade0' => array(self::BELONGS_TO, 'escolaridade', 'idescolaridade'),
-			'envio_emails' => array(self::HAS_MANY, 'envioEmail', 'idColaborador'),
-			'inscricaos' => array(self::HAS_MANY, 'inscricao', 'idColaborador'),
-			'instituicoesDirigidas' => array(self::HAS_MANY, 'instituicao', 'idResponsavel'),
-			'usuarios' => array(self::HAS_MANY, 'usuario', 'idColaborador'),
-			'vinculos' => array(self::HAS_MANY, 'vinculo', 'idColaborador'),
-		);
-	}
-
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
+	/** @return array customized attribute labels (name => label) */
 	public function attributeLabels() {
+
 		return array(
 
 			// Identificação
@@ -165,6 +112,41 @@ class colaborador extends CActiveRecord
 			'observacoes'     => 'Observações'
 			
 		);
+	}
+
+	/** Prepara alguns campos antes de serem validados. */
+	protected function beforeValidate(){
+
+		$this->cpf = preg_replace( '/[^0-9]/is', '', $this->cpf);
+
+		return true;
+	}
+
+	/** Prepara os campos antes de serem salvos no BD. */
+	protected function beforeSave(){
+
+		if (!parent::beforeSave())
+			return false;
+
+		// Recuperando dados da sessão
+		$session=Yii::app()->getSession();	
+		$usuario = $session["usuario"];
+		
+		// Ajustando campos
+		$this->data_cadastro = null;
+
+		$this->cep      = preg_replace( '/[^0-9]/is', '', $this->cep);
+		$this->cpf      = preg_replace( '/[^0-9]/is', '', $this->cpf);
+		$this->pispasep = preg_replace( '/[^0-9]/is', '', $this->pispasep);
+
+		// Cadastro externo
+		$this->tipo_cadastro = 1;
+
+		// Vincula no objeto 'colaborador' qual usuário o criou
+		if (isset($usuario))
+			$this->idusuario = $usuario->idUsuario;
+
+		return true;
 	}
 	
 	// Definição dos Status de Cadastro (no código mesmo pq não tem modelo)
@@ -217,33 +199,6 @@ class colaborador extends CActiveRecord
 	
 	public function behaviors(){
 		return array('datetimeI18NBehavior'=>array('class'=>'ext.DateTimeI18NBehavior'));
-	}
-	
-	/** Prepara os campos antes de serem salvos no BD. */
-	protected function beforeSave(){
-
-		if (!parent::beforeSave())
-			return false;
-
-		// Recuperando dados da sessão
-		$session=Yii::app()->getSession();	
-		$usuario = $session["usuario"];
-		
-		// Ajustando campos
-		$this->data_cadastro = null;
-
-		$this->cep      = preg_replace( '/[^0-9]/is', '', $this->cep);
-		$this->cpf      = preg_replace( '/[^0-9]/is', '', $this->cpf);
-		$this->pispasep = preg_replace( '/[^0-9]/is', '', $this->pispasep);
-
-		// Cadastro externo
-		$this->tipo_cadastro = 1;
-
-		// Vincula no objeto 'colaborador' qual usuário o criou
-		if (isset($usuario))
-			$this->idusuario = $usuario->idUsuario;
-
-		return true;
 	}
 	
 }
