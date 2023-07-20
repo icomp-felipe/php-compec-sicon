b81fd9c3ae7da168b50c6c3d1079c0aff185c001
Felipe André <fass@icomp.ufam.edu.br>
2023-07-20 10:35:24 -0400

    Adicionado link pro manual do PSC 2023-E12

--- protected/views/inscricaoPublico/confirmado.php
+++ protected/views/inscricaoPublico/confirmado.php
@@ -5,7 +5,7 @@
 	Obs.: não é necessário imprimir este manual, no dia do concurso o disponibilizaremos no formato impresso.
 </h3>
 
-<a href="/inscricao/arquivo/psc/2022-e12/manual-aplicador-psc2022-e12.pdf">Manual de Instruções do Aplicador (PSC 2022 - Etapas 1 e 2)</a>
+<a href="/inscricao/arquivo/psc/2023-e12/manual-aplicador-psc2023-e12.pdf">Manual de Instruções do Aplicador (PSC 2023 - Etapas 1 e 2)</a>
 
 <h2></h2>
 
